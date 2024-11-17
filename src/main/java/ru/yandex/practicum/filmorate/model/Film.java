0f96f5c07e7e91a6f25eb328ae770352b95e3805
Kristina Fedorova <krikri91@list.ru>
2024-11-17 14:46:05 +0300

    Initial commit

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,19 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import lombok.Data;
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+/**
+ * Film.
+ */
+@Getter
+@Setter
 public class Film {
-    private Long id;
-    @NotNull
-    @NotBlank
-    private String name;
-    private String description;
-    private LocalDate releaseDate;
-    @Positive
-    private Integer duration;
 }
